aaf155aa3954078232df103f5d7543e0bc84911d
jiho <jhpark@tsoft.co.kr>
2025-04-11 14:44:53 +0900

    personal newsletter api init commit

---
+++ src/test/java/com/gamjacoding/personalnewsletterapi/PersonalNewsletterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamjacoding.personalnewsletterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalNewsletterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
